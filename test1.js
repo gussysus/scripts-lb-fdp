96e5f6d28f4fb60ccf6e1d45752dbe5915a8f4ed
Oficiao amogus <106810659+gussysus@users.noreply.github.com>
2022-08-09 21:36:30 +0800

    hycraft fly test 1
    journey starts here

---
+++ test1.js
@@ -0,0 +1,22 @@
+/// api_version=2
+var script = registerScript({
+    name: "hycraft fly",
+    version: "0.1",
+    authors: ["rasheed"]
+});
+var C03PacketPlayer = Java.type("net.minecraft.network.play.client.C03PacketPlayer");
+var x;var y;var z;
+var S08PacketPlayerPosLook = Java.type('net.minecraft.network.play.server.S08PacketPlayerPosLook');
+var detected;
+var s08tick;
+script.registerModule({
+    name: "hycraft fly",
+    description: "",
+    category: "Movement",
+    settings: {
+    }
+}, function (module) {
+       module.on("update", function () {
+                  packet
+                  if (ModuleUsed.
+       }
